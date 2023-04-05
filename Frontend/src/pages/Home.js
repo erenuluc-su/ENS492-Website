8e864a70feddfbb57a20744282865e6990acc75c
Eren Uluç <erenuluc@Erens-MacBook-Pro-4.local>
2023-04-05 23:55:08 +0300

    Added buttons of partitions and the input that is needed

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,13 +1,78 @@
-import React from "react"
+import React, { useState } from "react"
 import { useHistory} from "react-router-dom";
+import styled from 'styled-components';
 
 export const Home = (props) => {
 
     let history = useHistory();
 
+    const types = ['Roger Ramanujan', 'Roger Ramanujan Gordon', 'Capparelli\'s Identity'];
+
+    const Button = styled.button``;
+
+    const ButtonToggle = styled(Button)`
+        opacity: 0.6;
+        ${({ active }) => active && `opacity: 1;`}
+    `;
+
+    const ButtonGroup = styled.div`
+        display: flex;
+    `;
+
+    const [active, setActive] = useState(types[0]);
+    const [mValue, setMValue] = useState("");
+    const [nValue, setNValue] = useState("");
+    const [kValue, setKValue] = useState("");
+
+    function ToggleGroup() {
+        return (
+          <ButtonGroup>
+            {types.map(type => (
+              <ButtonToggle
+                key={type}
+                active={active === type}
+                onClick={() => setActive(type)}
+              >
+                {type}
+              </ButtonToggle>
+            ))}
+          </ButtonGroup>
+        );
+    }
+
+    function Inside() {
+        if (active === "Roger Ramanujan"){
+            return (
+                <div className = "Roger Ramanujan">
+                    <input value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write value of m here" />
+                    <input value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write value of n here" />
+                </div>
+            );
+        }
+        else if (active === "Roger Ramanujan Gordon"){
+            return (
+                <div className = "Roger Ramanujan Gordon">
+                    <input value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write value of m here" />
+                    <input value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write value of n here" />
+                    <input value = {kValue} onChange={(e) => setKValue(e.target.value)} placeholder = "Write value of k here" />
+                </div>
+            );
+        }
+        if (active === "Capparelli\'s Identity"){
+            return (
+                <div className = "Capparelli\'s Identity">
+                    <input value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write value of m here" />
+                    <input value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write value of n here" />
+                </div>
+            );
+        }
+    }
+
     return (
-        <div className = "auth-form-container">
-            <h2>WELCOME TO Partionerator</h2>
+        <div className = "Partionerator">
+            <h1>WELCOME TO Partionerator</h1>
+            <ToggleGroup />
+            <Inside />
         </div>
     );
 }
