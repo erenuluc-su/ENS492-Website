d17b41fb9632b7991fde270c0900ea9bfa86cc69
Eren Uluç <erenuluc@Erens-MacBook-Pro-4.local>
2023-04-06 15:57:55 +0300

    Fix input bug

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -50,40 +50,38 @@ export const Home = (props) => {
         }
     }
 
-    function Inputs() {
-        if (active === "Roger Ramanujan" && option.length !== 0){
-            return (
-                <div className = "Roger Ramanujan">
-                    <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
-                    <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
-                </div>
-            );
-        }
-        else if (active === "Roger Ramanujan Gordon" && option.length !== 0){
-            return (
-                <div className = "Roger Ramanujan Gordon">
-                    <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
-                    <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
-                    <input type="text" value = {kValue} onChange={(e) => setKValue(e.target.value)} placeholder = "Write the value of k here!" />
-                </div>
-            );
-        }
-        if (active === "Capparelli\'s Identity" && option.length !== 0){
-            return (
-                <div className = "Capparelli\'s Identity">
-                    <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
-                    <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
-                </div>
-            );
-        }
-    }
-
     return (
         <div className = "Partionerator">
             <h1>WELCOME TO Partionerator</h1>
             <ToggleGroupPartitions />
             <ToggleGroupOptions />
-            <Inputs />
+            <div>
+                {(() => {
+                if (active === "Roger Ramanujan" && option.length !== 0) {
+                    return (
+                        <div className = "Roger Ramanujan">
+                        <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
+                        <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
+                        </div>
+                    )
+                } else if (active === "Roger Ramanujan Gordon" && option.length !== 0) {
+                    return (
+                        <div className = "Roger Ramanujan Gordon">
+                        <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
+                        <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
+                        <input type="text" value = {kValue} onChange={(e) => setKValue(e.target.value)} placeholder = "Write the value of k here!" />
+                        </div>
+                    )
+                } else if (active === "Capparelli\'s Identity" && option.length !== 0) {
+                    return (
+                        <div className = "Capparelli\'s Identity">
+                        <input type="text" value = {mValue} onChange={(e) => setMValue(e.target.value)} placeholder = "Write the value of m here!" />
+                        <input type="text" value = {nValue} onChange={(e) => setNValue(e.target.value)} placeholder = "Write the value of n here!" />
+                        </div>
+                    )
+                }
+                })()}
+            </div>
         </div>
     );
 }
